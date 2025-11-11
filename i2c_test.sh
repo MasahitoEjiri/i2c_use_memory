16f475eacae73c02888bee983592d4a766e518a7
Masahito Ejiri <ejiri-masahito@electronics.jtekt.co.jp>
2025-11-11 16:17:55 +0900

    パラメータ無しにおける全アドレスのデータ表示機能

--- i2c_test.sh
+++ i2c_test.sh
@@ -34,8 +34,69 @@ error()
     fi
 }
 
+#0~15を10進数➡16進数、A~Fを16進数➡10進数に変える
+change_1digit()
+{
+    local par=$1 #関数に入った引数を定義
+    list16=(0 1 2 3 4 5 6 7 8 9 A B C D E F)
+    for i in {0..15}
+    do
+        if [ $par = $i ];then #引数が0～15のときに当てはまる
+            echo ${list16[$i]} #その値（順番）に対応する16進数を出す
+        fi
+    done
+
+    list10=(10 11 12 13 14 15)
+    local count=0
+    for i in {A..F}
+    do
+        if [ $par = $i ];then #引数がA~Fのときに当てはまる
+            echo ${list10[$count]} #その値（順番）に対応する16進数を出す
+        fi
+        count=$(($count + 1)) #カウントアップ
+    done
+}
+
+#引数に与えられた2桁の16進数を10進数に変える
+change16to10()
+{
+    local par=$1 #関数に入った引数を定義
+    local par_leng=${#par} #桁数を定義
+    local par_0jou=${par:$((par_leng - 1)):1} #0乗目の値を取り出す（何番目：何文字）
+    local par_1jou=${par:$((par_leng - 2)):1} #1乗目の値取り出す
+
+    local ten_0jou=$(change_1digit $par_0jou) #一桁ずつ10進数に変換
+    local ten_1jou=$(change_1digit $par_1jou)
+
+    echo $((ten_0jou * 1 + ten_1jou * 16 )) #16の累乗分をかけて合算
+}
+
+#引数に与えられた255までの10進数を16進数に変える
+change10to16()
+{
+    local par=$1 #関数に入った引数を定義
+    local ten_0jou=$(($par % 16)) #16で割った余りを0乗の値に
+    local ten_1jou=$(($par / 16)) #16で割った商を1乗の値に
+
+    echo $(change_1digit $ten_1jou)$(change_1digit $ten_0jou) #1乗と0乗の値を16進数に変換し、連続で表示する
+}
+
+#指定したアドレスに至るまで、すべてのデータを表示する
+alladdress()
+{
+    fin_address="FF" #アドレス指定
+    local cnt_address=0
+    while [ $cnt_address -le $(change16to10 $fin_address) ] #指定したアドレスを10進数に変えて挿入
+    do
+        i2cget -y 1 0x50 0x$(change10to16 $cnt_address) #カウントアドレスを16進数に戻してi2cgetに挿入
+        cnt_address=`expr $cnt_address + 1` #カウントアップ
+    done
+}
+
+
 #メインループ
 if [ $arg_amount -eq 0 ]; then
+    alladdress
     exit
 elif [ $arg_amount -gt 1 ]; then
     error arg
